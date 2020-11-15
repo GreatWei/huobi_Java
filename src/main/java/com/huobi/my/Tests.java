e6403ba3da05b289c4d1f5b44f8cb273f1a42f32
GreatWei <1121412775@qq.com>
2020-11-16 00:03:35 +0800

    test

--- src/main/java/com/huobi/my/Tests.java
+++ src/main/java/com/huobi/my/Tests.java
@@ -1,17 +1,37 @@
 package com.huobi.my;
 
 import com.huobi.client.GenericClient;
+import com.huobi.client.MarketClient;
+import com.huobi.client.req.market.ReqCandlestickRequest;
+import com.huobi.client.req.market.SubCandlestickRequest;
 import com.huobi.constant.HuobiOptions;
+import com.huobi.constant.enums.CandlestickIntervalEnum;
+
+import java.text.SimpleDateFormat;
 
 public class Tests {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         GenericClient genericService = GenericClient.create(HuobiOptions.builder().build());
         for(int i=0;i<10;i++){
             System.out.println(i);
             long startTime = System.currentTimeMillis();
             Long serverTime = genericService.getTimestamp();
             System.out.println("server time:" + serverTime);
+            SimpleDateFormat format =  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            System.out.println("server time:" + format.format(serverTime));
             System.out.println(System.currentTimeMillis()-startTime);
         }
+
+        MarketClient marketClient = MarketClient.create(new HuobiOptions());
+
+        String symbol = "btcusdt";
+//        marketClient.subCandlestick(SubCandlestickRequest.builder()
+//                .symbol(symbol)
+//                .interval(CandlestickIntervalEnum.MIN1)
+//                .build(), (candlestick) -> {
+//
+//            System.out.println(candlestick.toString());
+//        });
     }
+
 }
