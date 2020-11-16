3e7aa521fe139f2fd2c9570cf16f6d09b6fb76c9
GreatWei <1121412775@qq.com>
2020-11-16 19:40:35 +0800

    test

--- src/main/java/com/huobi/my/DataFactor.java
+++ src/main/java/com/huobi/my/DataFactor.java
@@ -1,5 +1,9 @@
 package com.huobi.my;
 
+import com.huobi.model.market.Candlestick;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataFactor {
@@ -16,16 +20,26 @@ public class DataFactor {
         return d > c ? d : c;
     }
 
-    List<List<Double>> MACD(Records &records, size_t fastEMA =12, size_t slowEMA =26, size_t signalEMA =9) {
-        List<Double> ticks = records.Close();
-        List<Double> dif = _diff(_ema(ticks, fastEMA), _ema(ticks, slowEMA));
+    List<BigDecimal> _ticks(List<Candlestick> candlestickList){
+        if(candlestickList==null||candlestickList.size()==0)
+            return new ArrayList<BigDecimal>();
+        List<BigDecimal> doubleList = new ArrayList<BigDecimal>(50);
+        for (Candlestick candlestick:candlestickList)
+            doubleList.add(candlestick.getClose());
+        return doubleList;
+
+    }
+
+    Object MACD(List<Candlestick> records, int fastEMA , int slowEMA , int signalEMA ) {
+        List<BigDecimal> ticks =_ticks(records);
+        List<BigDecimal> dif = _diff(_ema(ticks, fastEMA), _ema(ticks, slowEMA));
         List<Double> signal = _ema(dif, signalEMA);
         List<Double> histogram = _diff(dif, signal);
         return {{dif, signal, histogram}};
     }
 
-    List<List<double>> KDJ(Records &records, size_t n =9, size_t k =3, size_t d =3) {
-        size_t length = records.size();
+    List<List<double>> KDJ(Records &records, int n =9, int k =3, int d =3) {
+        int length = records.size();
         List<double> RSV (length, 0);
         _set(RSV, 0, n - 1, NAN);
         List<double> K (length, 0);
@@ -35,8 +49,8 @@ public class DataFactor {
         List<double> hs = records.High();
         List<double> ls = records.Low();
 
-        for (size_t i = 0; i < length; i++) {
-            if (i >= size_t(n - 1)) {
+        for (int i = 0; i < length; i++) {
+            if (i >= int(n - 1)) {
                 double c = records[i].Close;
                 double h = _cmp(hs, i - (n - 1), i + 1, _cmp_max);
                 double l = _cmp(ls, i - (n - 1), i + 1, _cmp_min);
@@ -49,15 +63,15 @@ public class DataFactor {
             }
             J[i] = 3 * K[i] - 2 * D[i];
         }
-        for (size_t i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             K[i] = D[i] = J[i] = NAN;
         }
         return {{K, D, J}};
     }
 
-    List<double> RSI(Records &records, size_t period =14) {
-        size_t i = 0;
-        size_t n = period;
+    List<double> RSI(Records &records, int period =14) {
+        int i = 0;
+        int n = period;
         List<double> rsi (records.size(), 0);
         _set(rsi, 0, rsi.size(), NAN);
         if (records.size() < n) {
@@ -100,7 +114,7 @@ public class DataFactor {
         return rsi;
     }
 
-    List<double> ATR(Records &records, size_t period =14) {
+    List<double> ATR(Records &records, int period =14) {
         List<double> ret;
         if (records.size() == 0) {
             return ret;
@@ -108,7 +122,7 @@ public class DataFactor {
         List<double> R (records.size(), 0);
         double sum = 0.0;
         double n = 0.0;
-        for (size_t i = 0; i < records.size(); i++) {
+        for (int i = 0; i < records.size(); i++) {
             double TR = 0.0;
             if (i == 0) {
                 TR = records[i].High - records[i].Low;
@@ -131,7 +145,7 @@ public class DataFactor {
         if (records.size() == 0) {
             return R;
         }
-        for (size_t i = 0; i < records.size(); i++) {
+        for (int i = 0; i < records.size(); i++) {
             if (i == 0) {
                 R.push_back(records[i].Volume);
             } else if (records[i].Close >= records[i - 1].Close) {
@@ -143,19 +157,19 @@ public class DataFactor {
         return R;
     }
 
-    List<double> MA(Records &records, size_t period =9) {
+    List<double> MA(Records &records, int period =9) {
         return _sma(records.Close(), period);
     }
 
-    List<double> EMA(Records &records, size_t period =9) {
+    List<double> EMA(Records &records, int period =9) {
         return _ema(records.Close(), period);
     }
 
     List<List<double>, 3>
 
-    BOLL(Records &records, size_t period =20, double multiplier =2) {
+    BOLL(Records &records, int period =20, double multiplier =2) {
         List<double> S = records.Close();
-        size_t j = 0;
+        int j = 0;
         for (j = period - 1; j < S.size() && isnan(S[j]); j++) ;
         List<double> UP (S.size(), 0);
         List<double> MB (S.size(), 0);
@@ -164,9 +178,9 @@ public class DataFactor {
         _set(MB, 0, j, NAN);
         _set(DN, 0, j, NAN);
         double sum = 0;
-        for (size_t i = j; i < S.size(); i++) {
+        for (int i = j; i < S.size(); i++) {
             if (i == j) {
-                for (size_t k = 0; k < period; k++) {
+                for (int k = 0; k < period; k++) {
                     sum += S[k];
                 }
             } else {
@@ -174,7 +188,7 @@ public class DataFactor {
             }
             double ma = sum / period;
             double d = 0.0;
-            for (size_t k = i + 1 - period; k <= i; k++) {
+            for (int k = i + 1 - period; k <= i; k++) {
                 d += (S[k] - ma) * (S[k] - ma);
             }
             double stdev = sqrt(d / period);
@@ -189,9 +203,9 @@ public class DataFactor {
 
     List<List<double>, 3>
 
-    Alligator(Records &records, size_t jawLength =13, size_t teethLength =8, size_t lipsLength =5) {
+    Alligator(Records &records, int jawLength =13, int teethLength =8, int lipsLength =5) {
         List<double> ticks;
-        for (size_t i = 0; i < records.size(); i++) {
+        for (int i = 0; i < records.size(); i++) {
             ticks.push_back((records[i].High + records[i].Low) / 2);
         }
         List<double> jaw = _smma(ticks, jawLength);
@@ -203,13 +217,13 @@ public class DataFactor {
         return {{jaw, teeth, lips}};
     }
 
-    List<double> CMF(Records &records, size_t periods =20) {
+    List<double> CMF(Records &records, int periods =20) {
         List<double> ret;
         double sumD = 0.0;
         double sumV = 0.0;
         List<double> arrD;
         List<double> arrV;
-        for (size_t i = 0; i < records.size(); i++) {
+        for (int i = 0; i < records.size(); i++) {
             double d = (records[i].High == records[i].Low) ? 0 : (2 * records[i].Close - records[i].Low - records[i].High) / (records[i].High - records[i].Low) * records[i].Volume;
             arrD.push_back(d);
             arrV.push_back(records[i].Volume);
@@ -226,11 +240,11 @@ public class DataFactor {
         return ret;
     }
 
-    double Highest(List<double> records, size_t n) {
+    double Highest(List<double> records, int n) {
         return _filt(records, n, NAN, _cmp_max);
     }
 
-    double Lowest(List<double> records, size_t n) {
+    double Lowest(List<double> records, int n) {
         return _filt(records, n, NAN, _cmp_min);
     }
 
@@ -243,20 +257,20 @@ public class DataFactor {
         }
         double v = iv;
         double pos = n != 0 ? records.size() - _cmp_min(records.size() - 1, n) - 1 : 0;
-        for (size_t i = records.size() - 2; i >= pos; i--) {
+        for (int i = records.size() - 2; i >= pos; i--) {
             v = pfun(v, records[i]);
         }
         return v;
     }
 
-    List<double> _smma(List<double> S, size_t period) {
-        size_t length = S.size();
+    List<double> _smma(List<double> S, int period) {
+        int length = S.size();
         List<double> R (length, 0);
-        size_t j = _skip(S, period);
+        int j = _skip(S, period);
         _set(R, 0, j, NAN);
         if (j < length) {
             R[j] = _avg(S, j + 1);
-            for (size_t i = j + 1; i < length; i++) {
+            for (int i = j + 1; i < length; i++) {
                 R[i] = (R[i - 1] * (period - 1) + S[i]) / period;
             }
         }
@@ -265,34 +279,34 @@ public class DataFactor {
 
     List<double> _move_diff(List<double> a) {
         List<double> d;
-        for (size_t i = 1; i < a.size(); i++) {
+        for (int i = 1; i < a.size(); i++) {
             d.push_back(a[i] - a[i - 1]);
         }
         return d;
     }
 
-    List<double> _ema(List<double> S, size_t period) {
-        size_t length = S.size();
-        List<double> R (length, 0);
-        double multiplier = 2.0 / (period + 1);
-        size_t j = _skip(S, period);
-        _set(R, 0, j, NAN);
+    List<BigDecimal> _ema(List<BigDecimal> S, int period) {
+        int length = S.size();
+        List<BigDecimal> R = new ArrayList<BigDecimal>(length);
+        BigDecimal multiplier = new BigDecimal("2.0").divide(new BigDecimal(period + 1));
+        int j = _skip(S, period);
+        _set(R, 0, j, Double.NaN);
         if (j < length) {
             R[j] = _avg(S, j + 1);
-            for (size_t i = j + 1; i < length; i++) {
+            for (int i = j + 1; i < length; i++) {
                 R[i] = (S[i] - R[i - 1]) * multiplier + R[i - 1];
             }
         }
         return R;
     }
 
-    List<double> _sma(List<double> S, size_t period) {
+    List<double> _sma(List<double> S, int period) {
         List<double> R (S.size(), 0);
-        size_t j = _skip(S, period);
+        int j = _skip(S, period);
         _set(R, 0, j, NAN);
         if (j < S.size()) {
             double sum = 0;
-            for (size_t i = j; i < S.size(); i++) {
+            for (int i = j; i < S.size(); i++) {
                 if (i == j) {
                     sum = _sum(S, i + 1);
                 } else {
@@ -308,32 +322,27 @@ public class DataFactor {
         return R;
     }
 
-    double _sum(List<double> arr, size_t num) {
+    double _sum(List<Double> arr, int num) {
         double sum = 0.0;
-        for (size_t i = 0; i < num; i++) {
-            if (!isnan(arr[i])) {
+        for (int i = 0; i < num; i++) {
                 sum += arr[i];
-            }
         }
         return sum;
     }
 
-    List<double> _diff(List<double> a, List<double> b) {
-        List<double> d;
-        for (size_t i = 0; i < b.size(); i++) {
-            if (isnan(a[i]) || isnan(b[i])) {
-                d.push_back(NAN);
-            } else {
-                d.push_back(a[i] - b[i]);
-            }
+    List<BigDecimal> _diff(List<BigDecimal> a, List<BigDecimal> b) {
+        List<Double> d = new ArrayList<Double>();
+        for (int i = 0; i < b.size(); i++) {
+                double a=a[i].;
+                d.add(a[i] - b[i]);
         }
         return d;
     }
 
-    double _avg(List<double> arr, double num) {
-        size_t n = 0;
+    BigDecimal _avg(List<BigDecimal> arr, double num) {
+        int n = 0;
         double sum = 0.0;
-        for (size_t i = 0; i < num; i++) {
+        for (int i = 0; i < num; i++) {
             if (!isnan(arr[i])) {
                 sum += arr[i];
                 n++;
@@ -342,19 +351,17 @@ public class DataFactor {
         return sum / n;
     }
 
-    void _set(List<double> &arr, size_t start, size_t end, double value) {
-        size_t e = _cmp_min(arr.size(), end);
-        for (size_t i = start; i < e; i++) {
-            arr[i] = value;
+    void _set(List<BigDecimal> arr, int start, int end, double value) {
+        int e = arr.size()<end?arr.size(): end;
+        for (int i = start; i < e; i++) {
+          arr.set(i,new BigDecimal(value));
         }
     }
 
-    size_t _skip(List<double> arr, size_t period) {
-        size_t j = 0;
-        for (size_t k = 0; j < arr.size(); j++) {
-            if (!isnan(arr[j])) {
+    int _skip(List<BigDecimal> arr, int period) {
+        int j = 0;
+        for (int k = 0; j < arr.size(); j++) {
                 k++;
-            }
             if (k == period) {
                 break;
             }
@@ -362,12 +369,12 @@ public class DataFactor {
         return j;
     }
 
-    double _cmp(List<double> arr, size_t start, size_t end, double(*pfun) (
+    double _cmp(List<double> arr, int start, int end, double(*pfun) (
     double a, double b))
 
     {
         double v = arr[start];
-        for (size_t i = start; i < end; i++) {
+        for (int i = start; i < end; i++) {
             v = pfun(arr[i], v);
         }
         return v;
