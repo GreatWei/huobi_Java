5f0e2b405efd4cd3b9815b954d78d4c75b5c06a5
GreatWei <1121412775@qq.com>
2020-11-18 00:34:43 +0800

    test

--- src/main/java/com/huobi/my/DataFactor.java
+++ src/main/java/com/huobi/my/DataFactor.java
@@ -35,77 +35,107 @@ public class DataFactor {
         return doubleList;
     }
 
-    List<Double> MACD(List<Candlestick> records, int fastEMA, int slowEMA, int signalEMA) {
+    List<List<Double>> MACD(List<Candlestick> records, int fastEMA, int slowEMA, int signalEMA) {
         List<Double> ticks = _ticks(records);
         List<Double> dif = _diff(_ema(ticks, fastEMA), _ema(ticks, slowEMA));
         List<Double> signal = _ema(dif, signalEMA);
         List<Double> histogram = _diff(dif, signal);
-        return {{dif, signal, histogram}};
+        List<List<Double>> lists = new ArrayList<List<Double>>();
+        lists.add(dif);
+        lists.add(signal);
+        lists.add(histogram);
+        return lists;
     }
 
-    array<List<double>, 3>
 
-    KDJ(Records &records, int n =9, int k =3, int d =3) {
+
+   List<List<Double>> KDJ(List<Candlestick> records, int n, int k , int d) {
         int length = records.size();
-        List<double> RSV (length, 0);
-        _set(RSV, 0, n - 1, NAN);
-        List<double> K (length, 0);
-        List<double> D (length, 0);
-        List<double> J (length, 0);
+        //List<Double> RSV (length, 0);
+       List<Double> RSV = new ArrayList<Double>();
+       _zeros(RSV,length);
+        _set(RSV, 0, n - 1, Double.NaN);
+        List<Double> K = new ArrayList<>();
+        _zeros(K,length);
+        List<Double> D = new ArrayList<>();
+       _zeros(D,length);
+        List<Double> J = new ArrayList<>();
+       _zeros(J,length);
 
-        List<double> hs = records.High();
-        List<double> ls = records.Low();
+        List<Double> hs = new ArrayList<Double>();
+       List<Double> ls = new ArrayList<Double>();
+       for (Candlestick candlestick:records){
+            hs.add(candlestick.getHigh().doubleValue());
+            ls.add(candlestick.getLow().doubleValue());
+        }
 
         for (int i = 0; i < length; i++) {
-            if (i >= int(n - 1)){
-                double c = records[i].Close;
-                double h = _cmp(hs, i - (n - 1), i + 1, _cmp_max);
-                double l = _cmp(ls, i - (n - 1), i + 1, _cmp_min);
-                RSV[i] = h != l ? (100 * ((c - l) / (h - l))) : 100;
-                K[i] = (1 * RSV[i] + (k - 1) * K[i - 1]) / k;
-                D[i] = (1 * K[i] + (d - 1) * D[i - 1]) / d;
+            if (i >= (n - 1)){
+                double c = records.get(i).getClose().doubleValue();
+                double h = _cmp(hs, i - (n - 1), i + 1, (a,b)->{
+                    return a > b ? a : b;
+                });
+                double l = _cmp(ls, i - (n - 1), i + 1,(a,b)->{
+                    return a < b ? a : b;
+                });
+                RSV.set(i, h != l ? (100 * ((c - l) / (h - l))) : 100);
+                K.set(i , (1 * RSV.get(i) + (k - 1) * K.get(i - 1)) / k);
+                D.set(i, (1 * K.get(i) + (d - 1) * D.get(i - 1)) / d);
             } else{
-                K[i] = D[i] = 50;
-                RSV[i] = 0;
+                K.set(i,50.0);
+                D.set(i,50.0);
+                RSV.set(i ,0.0);
             }
-            J[i] = 3 * K[i] - 2 * D[i];
+            J.set(i, 3 * K.get(i) - 2 * D.get(i));
         }
         for (int i = 0; i < n - 1; i++) {
-            K[i] = D[i] = J[i] = NAN;
+            K.set(i,Double.NaN);
+            D.set(i,Double.NaN);
+            J.set(i,Double.NaN);
+
         }
-        return {{K, D, J}};
+        List<List<Double>> lists = new ArrayList<List<Double>>();
+        lists.add(K);
+       lists.add(D);
+       lists.add(J);
+        return lists;
     }
 
-    List<double> RSI(Records &records, int period =14) {
+    List<Double> RSI(List<Candlestick> records, int period) {
         int i = 0;
         int n = period;
-        List<double> rsi (records.size(), 0);
-        _set(rsi, 0, rsi.size(), NAN);
+        List<Double> rsi = new ArrayList<Double>();
+        _zeros(rsi,records.size());
+        _set(rsi, 0, rsi.size(), Double.NaN);
         if (records.size() < n) {
             return rsi;
         }
-        List<double> ticks = records.Close();
-        List<double> deltas = _move_diff(ticks);
-        List<double> seed (deltas.begin(), deltas.begin() + n);
+        List<Double> ticks = new ArrayList<>();
+        for(Candlestick candlestick:records){
+            ticks.add(candlestick.getClose().doubleValue());
+        }
+        List<Double> deltas = _move_diff(ticks);
+      //  List<Double> seed (deltas.begin(), deltas.begin() + n);
+            List<Double> seed = deltas.subList(0,n);
         double up = 0.0;
         double down = 0.0;
         for (i = 0; i < seed.size(); i++) {
-            if (seed[i] >= 0) {
-                up += seed[i];
+            if (seed.get(i) >= 0) {
+                up += seed.get(i);
             } else {
-                down += seed[i];
+                down += seed.get(i);
             }
         }
         up /= n;
         down /= n;
         down = -down;
         double rs = down != 0 ? up / down : 0;
-        rsi[n] = 100 - 100 / (1 + rs);
+        rsi.set(n, 100 - 100 / (1 + rs));
         double delta = 0.0;
         double upval = 0.0;
         double downval = 0.0;
         for (i = n + 1; i < ticks.size(); i++) {
-            delta = deltas[i - 1];
+            delta = deltas.get(i - 1);
             if (delta > 0) {
                 upval = delta;
                 downval = 0;
@@ -116,25 +146,26 @@ public class DataFactor {
             up = (up * (n - 1) + upval) / n;
             down = (down * (n - 1) + downval) / n;
             rs = up / down;
-            rsi[i] = 100 - 100 / (1 + rs);
+            rsi.set(i, 100 - 100 / (1 + rs));
         }
         return rsi;
     }
 
-    List<double> ATR(Records &records, int period =14) {
-        List<double> ret;
+    List<Double> ATR(List<Candlestick> records, int period ) {
+        List<Double> R = new ArrayList<>();
         if (records.size() == 0) {
-            return ret;
+            return R;
         }
-        List<double> R (records.size(), 0);
+       // List<double> R (records.size(), 0);
+        _zeros(R,records.size());
         double sum = 0.0;
         double n = 0.0;
         for (int i = 0; i < records.size(); i++) {
             double TR = 0.0;
             if (i == 0) {
-                TR = records[i].High - records[i].Low;
+                TR = records.get(i).getHigh().doubleValue() - records.get(i).getLow().doubleValue();
             } else {
-                TR = _cmp_max(records[i].High - records[i].Low, abs(records[i].High - records[i - 1].Close), abs(records[i - 1].Close - records[i].Low));
+                TR = _cmp_max(records.get(i).getHigh().doubleValue() - records.get(i).getLow().doubleValue(), Math.abs(records.get(i).getHigh().doubleValue() - records.get(i - 1).getClose().doubleValue()), Math.abs(records.get(i - 1).getClose().doubleValue() - records.get(i).getLow().doubleValue()));
             }
             sum += TR;
             if (i < period) {
@@ -142,69 +173,91 @@ public class DataFactor {
             } else {
                 n = (((period - 1) * n) + TR) / period;
             }
-            R[i] = n;
+            R.set(i, n);
         }
         return R;
     }
 
-    List<double> OBV(Records &records) {
-        List<double> R;
+    List<Double> OBV(List<Candlestick> records) {
+        List<Double> R;
         if (records.size() == 0) {
-            return R;
+            return new ArrayList<>();
         }
+        R = new ArrayList<Double>();
+        //_zeros(R,records.size());
         for (int i = 0; i < records.size(); i++) {
             if (i == 0) {
-                R.push_back(records[i].Volume);
-            } else if (records[i].Close >= records[i - 1].Close) {
-                R.push_back(R[i - 1] + records[i].Volume);
+                R.add(records.get(i).getVol().doubleValue());
+            } else if (records.get(i).getClose().doubleValue() >= records.get(i - 1).getClose().doubleValue()) {
+                R.add(R.get(i - 1) + records.get(i).getVol().doubleValue());
             } else {
-                R.push_back(R[i - 1] - records[i].Volume);
+                R.add(R.get(i - 1) - records.get(i).getVol().doubleValue());
             }
         }
         return R;
     }
 
-    List<double> MA(Records &records, int period =9) {
-        return _sma(records.Close(), period);
+    List<Double> MA(List<Candlestick> records, int period ) {
+        List<Double> closeList = new ArrayList<Double>();
+        for(Candlestick candlestick:records){
+            closeList.add(candlestick.getClose().doubleValue());
+        }
+        return _sma(closeList, period);
     }
 
-    List<double> EMA(Records &records, int period =9) {
-        return _ema(records.Close(), period);
+    List<Double> EMA(List<Candlestick> records, int period) {
+        List<Double> closeList = new ArrayList<Double>();
+        for(Candlestick candlestick:records){
+            closeList.add(candlestick.getClose().doubleValue());
+        }
+        return _ema(closeList, period);
     }
 
 
-    List<List<Double>> BOLL(List<d> &records, int period =20, double multiplier =2) {
-        List<double> S = records.Close();
+    List<List<Double>> BOLL(List<Candlestick> records, int period , double multiplier) {
+        List<Double> S = new ArrayList<Double>();
+        for(Candlestick candlestick:records){
+            S.add(candlestick.getClose().doubleValue());
+        }
         int j = 0;
-        for (j = period - 1; j < S.size() && isnan(S[j]); j++) ;
-        List<double> UP (S.size(), 0);
-        List<double> MB (S.size(), 0);
-        List<double> DN (S.size(), 0);
-        _set(UP, 0, j, NAN);
-        _set(MB, 0, j, NAN);
-        _set(DN, 0, j, NAN);
+        for (j = period - 1; j < S.size() && Double.isNaN(S.get(j)); j++) ;
+
+        List<Double> UP = new ArrayList<Double>();
+        _zeros(UP,S.size());
+        List<Double> MB = new ArrayList<Double>();
+        _zeros(MB,S.size());
+        List<Double> DN = new ArrayList<Double>();
+        _zeros(DN,S.size());
+        _set(UP, 0, j, Double.NaN);
+        _set(MB, 0, j, Double.NaN);
+        _set(DN, 0, j, Double.NaN);
         double sum = 0;
         for (int i = j; i < S.size(); i++) {
             if (i == j) {
                 for (int k = 0; k < period; k++) {
-                    sum += S[k];
+                    sum += S.get(k);
                 }
             } else {
-                sum = sum + S[i] - S[i - period];
+                sum = sum + S.get(i) - S.get(i - period);
             }
             double ma = sum / period;
             double d = 0.0;
             for (int k = i + 1 - period; k <= i; k++) {
-                d += (S[k] - ma) * (S[k] - ma);
+                d += (S.get(k) - ma) * (S.get(k) - ma);
             }
-            double stdev = sqrt(d / period);
+            double stdev = Math.sqrt(d / period);
             double up = ma + (multiplier * stdev);
             double dn = ma - (multiplier * stdev);
-            UP[i] = up;
-            MB[i] = ma;
-            DN[i] = dn;
+            UP.set(i, up);
+            MB.set(i,  ma);
+            DN.set(i, dn);
         }
-        return {{UP, MB, DN}};
+        List<List<Double>> result = new ArrayList<List<Double>>();
+        result.add(UP);
+        result.add(MB);
+        result.add(DN);
+        //return {{UP, MB, DN}};
+        return result;
     }
 
 
@@ -405,6 +458,12 @@ public class DataFactor {
             v = methods.methodsChose(arr.get(i).doubleValue(), v);
         }
         return v;
+    }
+    void _zeros(List<Double> list,int len){
+
+        for(int i=0;i<len;i++){
+            list.add(0.0);
+        }
     }
 }
 
