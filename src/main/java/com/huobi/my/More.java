cf52c71713dd20181fd77aeff12628afe269a798
GreatWei <1121412775@qq.com>
2020-11-30 18:02:25 +0800

    test

--- src/main/java/com/huobi/my/More.java
+++ src/main/java/com/huobi/my/More.java
@@ -45,62 +45,67 @@ public class More {
         final BigDecimal service_charge = new BigDecimal("0.002");
 
         while (true) {
-            startTime = System.currentTimeMillis();
-            List<Candlestick> listMIN15 = marketClient.getCandlestick(CandlestickRequest.builder()
-                    .symbol(symbol)
-                    .interval(CandlestickIntervalEnum.MIN15)
-                    .size(20)
-                    .build());
-            List<Candlestick> listDAY1 = marketClient.getCandlestick(CandlestickRequest.builder()
-                    .symbol(symbol)
-                    .interval(CandlestickIntervalEnum.DAY1)
-                    .size(1)
-                    .build());
-            k_day = listDAY1.get(0);
+            try {
+                startTime = System.currentTimeMillis();
+                List<Candlestick> listMIN15 = marketClient.getCandlestick(CandlestickRequest.builder()
+                        .symbol(symbol)
+                        .interval(CandlestickIntervalEnum.MIN15)
+                        .size(20)
+                        .build());
+                List<Candlestick> listDAY1 = marketClient.getCandlestick(CandlestickRequest.builder()
+                        .symbol(symbol)
+                        .interval(CandlestickIntervalEnum.DAY1)
+                        .size(1)
+                        .build());
+                k_day = listDAY1.get(0);
 
-            double MA5 = dataFactor.MA(listMIN15, 5).get(4);
-            double MA10 = dataFactor.MA(listMIN15, 10).get(9);
-            double MA20 = dataFactor.MA(listMIN15, 20).get(19);
-            if (single && MA5 > MA10) {
-                List<MarketTrade> marketTradeList = marketClient.getMarketTrade(MarketTradeRequest.builder().symbol(symbol).build());
-                for (int i = 0; i < marketTradeList.size(); i++) {
-                    if (i == 0) {
-                        min = marketTradeList.get(i).getPrice();
-                    } else {
-                        min = min.min(marketTradeList.get(i).getPrice());
+                double MA5 = dataFactor.MA(listMIN15, 5).get(4);
+                double MA10 = dataFactor.MA(listMIN15, 10).get(9);
+                double MA20 = dataFactor.MA(listMIN15, 20).get(19);
+                if (single && MA5 > MA10) {
+                    List<MarketTrade> marketTradeList = marketClient.getMarketTrade(MarketTradeRequest.builder().symbol(symbol).build());
+                    for (int i = 0; i < marketTradeList.size(); i++) {
+                        if (i == 0) {
+                            min = marketTradeList.get(i).getPrice();
+                        } else {
+                            min = min.min(marketTradeList.get(i).getPrice());
+                        }
+                    }
+                    if ((k_day.getHigh().multiply(service_charge).add(min.multiply(service_charge)).add(min)).compareTo(k_day.getHigh()) <= 0) {
+                        log.error("buy:" + min);
+                        buy = min;
+                        single = false;
+                    }
+                } else if (!single && MA10 > MA5) {
+                    List<MarketTrade> marketTradeList = marketClient.getMarketTrade(MarketTradeRequest.builder().symbol(symbol).build());
+                    for (int i = 0; i < marketTradeList.size(); i++) {
+                        if (i == 0) {
+                            max = marketTradeList.get(i).getPrice();
+                        } else {
+                            max = max.max(marketTradeList.get(i).getPrice());
+                        }
+                    }
+                    sub = max.subtract(buy).subtract(buy.multiply(service_charge)).subtract(max.multiply(service_charge));
+                    if (sub.compareTo(new BigDecimal("0.0")) > 0) {
+                        sell = max;
+                        log.error("sell:" + sell);
+                        sum = sum.add(sub);
+                        log.error("profit:" + sum);
+                        single = true;
                     }
                 }
-                if ((k_day.getHigh().multiply(service_charge).add(min.multiply(service_charge)).add(min)).compareTo(k_day.getHigh()) <= 0) {
-                    log.error("buy:" + min);
-                    buy = min;
-                    single = false;
-                }
-            } else if (!single && MA10 > MA5) {
-                List<MarketTrade> marketTradeList = marketClient.getMarketTrade(MarketTradeRequest.builder().symbol(symbol).build());
-                for (int i = 0; i < marketTradeList.size(); i++) {
-                    if (i == 0) {
-                        max = marketTradeList.get(i).getPrice();
-                    } else {
-                        max = max.max(marketTradeList.get(i).getPrice());
-                    }
-                }
-                sub = max.subtract(buy).subtract(buy.multiply(service_charge)).subtract(max.multiply(service_charge));
-                if (sub.compareTo(new BigDecimal("0.0")) > 0) {
-                    sell = max;
-                    log.error("sell:" + sell);
-                    sum = sum.add(sub);
-                    log.error("profit:" + sum);
-                    single = true;
-                }
-            }
 //            System.err.println(listDAY1.get(0));
 //            System.err.println(MA5 + "," + MA10 + "," + MA20);
-            endTime = System.currentTimeMillis();
-            if ((endTime - startTime) > 100) {
-                continue;
-            } else {
-                Thread.sleep(endTime - startTime);
+                endTime = System.currentTimeMillis();
+                if ((endTime - startTime) > 100) {
+                    continue;
+                } else {
+                    Thread.sleep(endTime - startTime);
+                }
+            }catch (Exception e){
+                log.error("Exception: "+e.toString());
             }
+
         }
     }
 }
